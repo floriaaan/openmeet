0d1fef832cd07e996812f39bdbeef5f2088fd4a9
Randazzo Pierre <pierre.randazzo@viacesi.fr>
2020-02-26 09:22:03 +0100

    controller et model space

---
+++ app/Model/Notification.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Notification extends Model
+{
+    //
+}
