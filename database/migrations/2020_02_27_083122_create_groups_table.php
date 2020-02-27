4663b6dda908591abab3a5e8ca4e566bc6fffc29
Randazzo Pierre <pierre.randazzo@viacesi.fr>
2020-02-27 09:52:30 +0100

    table group schema

---
+++ database/migrations/2020_02_27_083122_create_groups_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->bigIncrements('id');
+           /*
+           todo $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')
+                  ->references('id')
+                 ->on('users'); */
+            $table->string('name',64);
+            $table->string('picrepo',255);
+            $table->string('picname',255);
+            $table->date('datecreate');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('groups');
+    }
+}
