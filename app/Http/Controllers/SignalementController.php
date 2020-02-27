c4e517a736f43a993f4e770aad036f4669e4d3bf
Randazzo Pierre <pierre.randazzo@viacesi.fr>
2020-02-27 13:50:33 +0100

    model & controller signalement

--- app/Http/Controllers/SignalementController.php
+++ app/Http/Controllers/SignalementController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Signalement;
 
-class Signalement extends Controller
+class SignalementController extends Controller
 {
     //
 }
