48eb6acf4eb8f1d10e8017e4cd9e5a85dc669ca9
Florian Leroux <florian.leroux3@laposte.net>
2020-02-27 10:16:01 +0100

    Added Messages migration

---
+++ database/migrations/2020_02_27_091525_create_messages_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->dateTime('date');
+            $table->text('content');
+            $table->bigInteger('receiver'); //TODO: foreign key
+            $table->bigInteger('sender'); //TODO: foreign key
+            $table->boolean('isread');
+            $table->boolean('forgroup');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
