83cdf9e025f0464b361489d58004d532ea1d763e
Randazzo Pierre <pierre.randazzo@viacesi.fr>
2020-02-27 16:06:34 +0100

    model group/event

--- app/Model/Event.php
+++ app/Model/Event.php
@@ -1,40 +1,90 @@
 <?php
 
 namespace App;
-
+use Carbon\Carbon;
+use Faker\Provider\DateTime;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 
 class Event extends Model
 {
 
-    public function index()
-    {
-        //
-    }
+    protected $fillable = [
+        'idevent',
+        'idgroup',
+        'name',
+        'datefrom',
+        'dateto',
+        'posX',
+        'posY',
+        'country',
+        'zip',
+        'city',
+        'numstreet',
+        'street',
+        'desc',
+        ];
 
-    public function create()
+    public function create($groupId,$name,$datefrom,$dateto,$country,$zip,$city,$numstreet,$street,$desc)
     {
-        //
+        $query=DB::table('EVENTS')
+            ->insert([
+                'id_group'=>$groupId,
+                'name'=>$name,
+                'datefrom'=>$datefrom,
+                'dateto'=>$dateto,
+                'country'=>$country,
+                'zip'=>$zip,
+                'city'=>$city,
+                'numstreet'=>$numstreet,
+                'street'=>$street,
+                'description'=>$desc,
+            ]);
     }
-
 
     public function show()
     {
-        //
+        $events  = DB::table('events')
+            ->select('*')
+            ->where('id_group')
+            ->get();
     }
 
-
-    public function edit()
+    public function showall($groupId)
     {
-        //
+            $event=DB::table('events')
+                ->select('*')
+                ->where('id_group',"=",$groupId)
+                ->get();
+            $eventsArray=$event;
+            $listevent=[];
+            foreach ($eventsArray as $eventSQL)
+            {
+                $listevent[]=$eventSQL;
+            }
+            return $listevent;
     }
 
-    public function Delete()
+    public function edit($groupId)
     {
-        //
+        $events= DB::table('events')
+            ->update($groupId);
     }
-    //
+
+    public function delete($groupId)
+    {
+        $query=DB::table('events')
+            ->delete($groupId);
+    }
+
+    public function DeleteTime($eventID)
+    {
+        $timer = DateTime::dateTime(now());
+        $events =DB::table('events')
+            ->where('dateto', '=', value($timer))
+            ->delete($eventID);
+    }
+
 }
