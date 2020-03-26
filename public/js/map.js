f21cb43b7e219137b273842fcf22cb0ae4098e3a
Florian Leroux <florian.leroux3@laposte.net>
2020-03-26 14:04:21 +0100

    Fixes

--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,7 @@
 var mymap = L.map('map').setView([46.728427, 2.611003], 4);
 
 function displayMap() {
-    L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw',
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
         {
             maxZoom: 18,
             attribution: 'OpenMeet',
